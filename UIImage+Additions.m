b8ac2bec0c6af23495e6836b377d250be556b900
Joan Martin <vilanovi@gmail.com>
2013-05-31 09:40:45 +0700

    Adding MD5 Hashing for image key generation

--- UIImage+Additions.m
+++ UIImage+Additions.m
@@ -4,7 +4,34 @@
 //  Take a look to my repos at http://github.com/vilanovi
 //
 #import "UIImage+Additions.h"
-#import "NSString+MD5Hashing.h"
+
+@interface NSString (MD5Hashing)
+
+- (NSString*)md5;
+
+@end
+
+#import <CommonCrypto/CommonDigest.h>
+
+@implementation NSString (MD5Hashing)
+
+- (NSString *)md5
+{
+    const char *cStr = [self UTF8String];
+    unsigned char result[16];
+    
+    CC_MD5(cStr, strlen(cStr), result);
+    
+    return [NSString stringWithFormat:
+			@"%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x",
+			result[0], result[1], result[2], result[3],
+			result[4], result[5], result[6], result[7],
+			result[8], result[9], result[10], result[11],
+			result[12], result[13], result[14], result[15]
+			];
+}
+
+@end
 
 const UICornerInset UICornerInsetZero = {0.0f, 0.0f, 0.0f, 0.0f};
 
